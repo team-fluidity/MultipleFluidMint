127b0ddc77e07942986e4c471df30482ee00c4c0
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2022-03-03 23:02:37 -0800

    initial build

---
+++ arguments.js
@@ -0,0 +1,7 @@
+module.exports = [
+    'budgetNFT', 
+    'BGT', 
+    '0xEB796bdb90fFA0f28255275e16936D25d3418603',
+    '0x49e565Ed1bdc17F3d220f72DF0857C26FA83F873',
+    '0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f'
+    ];
